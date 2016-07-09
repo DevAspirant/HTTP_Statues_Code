70047a3ae40b8e2df01ff9b909c44722288f0257
ammar falmban <devAspirant@gmail.com>
2016-07-09 23:17:55 +0300

    Normal Switch
    using a normal switch

--- main.swift
+++ main.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
-// adding 
-var HTTP_Statues_Code: String
+// adding
+var HTTP_Statues_Code: Int = 404
+var ErrorStirng: String
+
+// using switch 
+switch HTTP_Statues_Code {
+
+case 400:print("BAD REQUEST")
+case 401:print("UNAUTHORIZED")
+case 403:print("FORBIDDEN")
+case 404:print("NOT FOUND")
+    
+default: print("NONE")
+}
 
 
